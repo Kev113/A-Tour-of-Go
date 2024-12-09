6028b475e00a217a8e17a545dd089937e8765ffe
Kev113 <kev21113@gmail.com>
2024-12-09 06:51:53 +0100

    Update the contens of A-Tour-of-Go

--- moretypes/1.go
+++ moretypes/1.go
@@ -1,17 +1,60 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	i, j := 42, 2701
+
+// 	p := &i
+// 	fmt.Println(p, *p)
+
+// 	*p = 21
+// 	fmt.Println(i)
+
+// 	p = &j
+// 	*p = *p / 37
+// 	fmt.Println(j)
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	i, j := 42, 2701
 
-	p := &i
-	fmt.Println(p, *p)
+	const s = "สวัสดี"
 
-	*p = 21
-	fmt.Println(i)
+	fmt.Println("Len:", len(s))
 
-	p = &j
-	*p = *p / 37
-	fmt.Println(j)
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
+
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+
+	fmt.Println("\nUsing DecodeRuneInString")
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%#U starts at %d\n", runeValue, i)
+		w = width
+
+		examineRune(runeValue)
+	}
+}
+
+func examineRune(r rune) {
+
+	if r == 't' {
+		fmt.Println("found tee")
+	} else if r == 'ส' {
+		fmt.Println("found so sua")
+	}
 }
